960ae8b1c67de0d66f85907627d1a5c67b27dee7
koyif <koyif@users.noreply.github.com>
2025-07-16 09:11:31 +0200

    INC-2: fix linter remarks

--- internal/agent/storage/storage.go
+++ internal/agent/storage/storage.go
@@ -4,20 +4,20 @@ type MetricStorage struct {
 	metrics map[string]float64
 }
 
-func New() MetricStorage {
-	return MetricStorage{
+func New() *MetricStorage {
+	return &MetricStorage{
 		metrics: make(map[string]float64),
 	}
 }
 
-func (s MetricStorage) Metrics() map[string]float64 {
+func (s *MetricStorage) Metrics() map[string]float64 {
 	return s.metrics
 }
 
-func (s MetricStorage) Store(metric string, value float64) {
+func (s *MetricStorage) Store(metric string, value float64) {
 	s.metrics[metric] = value
 }
 
-func (s MetricStorage) Clean() {
+func (s *MetricStorage) Clean() {
 	s.metrics = make(map[string]float64)
 }
