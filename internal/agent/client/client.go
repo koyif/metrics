e69e85b0c312faa8cc3cd9e9867c60236eb7f750
koyif <koyif@users.noreply.github.com>
2025-07-16 09:05:33 +0200

    INC-2: fix linter remarks

--- internal/agent/client/client.go
+++ internal/agent/client/client.go
@@ -3,46 +3,46 @@ package client
 import (
 	"fmt"
 	"github.com/koyif/metrics/internal/agent/config"
-	"io"
 	"net/http"
 	"net/url"
 )
 
 type MetricsClient struct {
 	httpClient *http.Client
-	baseUrl    *url.URL
+	baseURL    *url.URL
 }
 
 func New(cfg *config.Config, c *http.Client) (*MetricsClient, error) {
 	const op = "MetricsClient.New"
-	baseUrl, err := url.Parse(cfg.Server.Addr)
+	baseURL, err := url.Parse(cfg.Server.Addr)
 	if err != nil {
 		return nil, fmt.Errorf("%s: error creating MetricsClient: %w", op, err)
 	}
 
-	baseUrl = baseUrl.JoinPath("update")
+	baseURL = baseURL.JoinPath("update")
 
 	return &MetricsClient{
 		httpClient: c,
-		baseUrl:    baseUrl,
+		baseURL:    baseURL,
 	}, nil
 }
 
 func (c *MetricsClient) Send(metricType, metricName, value string) error {
 	const op = "MetricsClient.Send"
-	u := c.baseUrl.
+	u := c.baseURL.
 		JoinPath(metricType).
 		JoinPath(metricName).
 		JoinPath(value)
 
 	response, err := c.httpClient.Post(u.String(), "text/plain", nil)
 	if err != nil {
+		if response != nil && response.Body != nil {
+			_ = response.Body.Close()
+		}
 		return err
 	}
 
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(response.Body)
+	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		return fmt.Errorf("%s: incorrect response status: %d", op, response.StatusCode)
