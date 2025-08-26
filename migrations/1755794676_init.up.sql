931174397a870edfae512384fe48c4324ddd20bb
koyif <koyif@users.noreply.github.com>
2025-08-26 02:13:55 +0200

    INC-12: fix: change datatypes in DB

--- migrations/1755794676_init.up.sql
+++ migrations/1755794676_init.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS metrics
     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     metric_name TEXT    NOT NULL UNIQUE,
     metric_type TEXT    NOT NULL,
-    metric_value        FLOAT,
-    metric_delta        INTEGER,
+    metric_value        DOUBLE PRECISION,
+    metric_delta        BIGINT,
     updated_at          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
